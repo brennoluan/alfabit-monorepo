94b73bf7372556ac8453a8a5d4a09cd582e4df0a
Brenno Luan <brennoluan8530@gmail.com>
2026-06-02 23:17:12 -0300

    feat: add typography tokens for consistent text styling in alfabit-tokens package

---
+++ packages/alfabit-tokens/src/typography.ts
@@ -0,0 +1,107 @@
+export const typography = {
+  headline: {
+    h1: {
+      fontSize: "56px",
+      fontFamily: "Mulish",
+      fontWeight: "800",
+      lineHeight: "62px",
+    },
+    h2: {
+      fontSize: "48px",
+      fontFamily: "Mulish",
+      fontWeight: "800",
+      lineHeight: "56px",
+    },
+    h3: {
+      fontSize: "40px",
+      fontFamily: "Mulish",
+      fontWeight: "800",
+      lineHeight: "48px",
+    },
+    subtitle1: {
+      fontSize: "32px",
+      fontFamily: "Mulish",
+      fontWeight: "800",
+      lineHeight: "40px",
+    },
+    subtitle2: {
+      fontSize: "24px",
+      fontFamily: "Mulish",
+      fontWeight: "800",
+      lineHeight: "30px",
+    },
+  },
+  text: {
+    largeBold: {
+      fontSize: "18px",
+      fontFamily: "Mulish",
+      fontWeight: "700",
+      lineHeight: "24px",
+    },
+    large: {
+      fontSize: "18px",
+      fontFamily: "Mulish",
+      fontWeight: "400",
+      lineHeight: "24px",
+    },
+    mediumBold: {
+      fontSize: "16px",
+      fontFamily: "Mulish",
+      fontWeight: "700",
+      lineHeight: "20px",
+    },
+    medium: {
+      fontSize: "16px",
+      fontFamily: "Mulish",
+      fontWeight: "400",
+      lineHeight: "20px",
+    },
+    smallBold: {
+      fontSize: "14px",
+      fontFamily: "Mulish",
+      fontWeight: "700",
+      lineHeight: "18px",
+    },
+    small: {
+      fontSize: "14px",
+      fontFamily: "Mulish",
+      fontWeight: "400",
+      lineHeight: "18px",
+    },
+    extraSmallBold: {
+      fontSize: "12px",
+      fontFamily: "Mulish",
+      fontWeight: "700",
+      lineHeight: "16px",
+    },
+    extraSmall: {
+      fontSize: "12px",
+      fontFamily: "Mulish",
+      fontWeight: "400",
+      lineHeight: "16px",
+    },
+  },
+  link: {
+    buttonLarge: {
+      fontSize: "18px",
+      fontFamily: "Mulish",
+      fontWeight: "800",
+      lineHeight: "24px",
+      letterSpacing: "0.36px",
+    },
+    buttonMedium: {
+      fontSize: "16px",
+      fontFamily: "Mulish",
+      fontWeight: "800",
+      lineHeight: "24px",
+      letterSpacing: "0.32px",
+    },
+    buttonSmall: {
+      fontSize: "14px",
+      fontFamily: "Mulish",
+      fontWeight: "800",
+      lineHeight: "16px",
+      letterSpacing: "0.28px",
+    },
+  },
+};
