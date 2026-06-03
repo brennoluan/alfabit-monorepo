c9338acd7e9d3d1444a1c7658454ef3ec72b9d30
Brenno Luan <brennoluan8530@gmail.com>
2026-06-02 22:33:56 -0300

    feat: integrate @brennoluan/alfabit-tokens into alfabit-button for dynamic styling

--- packages/alfabit-tokens/src/colors.ts
+++ packages/alfabit-tokens/src/colors.ts
@@ -1,30 +1,32 @@
-export const colors = {
-  bgLight: "#FFFFFF",
-  bgDark: "#F8F8F8",
+export const background = {
+  light: "#FFFFFF",
+  dark: "#F8F8F8",
 };
 
-export const text = {
-  gray: {
-    primary: "#181818",
-    secondary: "#5c5c5c",
-    tertiary: "#747474",
-    icon: "#7c7b7b",
-    border: "#d7d7d7",
-    divider: "#ededed",
-    disabled: "#8a8a8a",
-  },
-  blue: {
-    primary: "#2d5bff",
-    secondary: "#6284fd",
-    tertiary: "#96ADFF",
-    quaternary: "#ECF0FF",
-    hover: "#1B4AF0",
-    click: "#002ED0",
-  },
-  violet: {
-    primary: "#AF4BFE",
-    secondary: "#BE74F9",
-    tertiary: "#E2BDFF",
-    quaternary: "#F5E8FF",
-  },
+export const gray = {
+  primary: "#181818",
+  secondary: "#5c5c5c",
+  tertiary: "#747474",
+  icon: "#7c7b7b",
+  border: "#d7d7d7",
+  divider: "#ededed",
+  disabled: "#8a8a8a",
+  white: "#FFFFFF",
+  black: "#000000",
+};
+
+export const blue = {
+  primary: "#2d5bff",
+  secondary: "#6284fd",
+  tertiary: "#96ADFF",
+  quaternary: "#ECF0FF",
+  hover: "#1B4AF0",
+  click: "#002ED0",
+};
+
+export const violet = {
+  primary: "#AF4BFE",
+  secondary: "#BE74F9",
+  tertiary: "#E2BDFF",
+  quaternary: "#F5E8FF",
 };
