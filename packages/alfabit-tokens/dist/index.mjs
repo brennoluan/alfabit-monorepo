985a86baa5e1ff2fccfb500a7d37fa4d7aea1bed
Brenno Luan <brennoluan8530@gmail.com>
2026-06-02 22:21:23 -0300

    feat: add TypeScript configuration and color tokens for @brennoluan/alfabit-tokens package

---
+++ packages/alfabit-tokens/dist/index.mjs
@@ -0,0 +1,34 @@
+// src/colors.ts
+var colors = {
+  bgLight: "#FFFFFF",
+  bgDark: "#F8F8F8"
+};
+var text = {
+  gray: {
+    primary: "#181818",
+    secondary: "#5c5c5c",
+    tertiary: "#747474",
+    icon: "#7c7b7b",
+    border: "#d7d7d7",
+    divider: "#ededed",
+    disabled: "#8a8a8a"
+  },
+  blue: {
+    primary: "#2d5bff",
+    secondary: "#6284fd",
+    tertiary: "#96ADFF",
+    quaternary: "#ECF0FF",
+    hover: "#1B4AF0",
+    click: "#002ED0"
+  },
+  violet: {
+    primary: "#AF4BFE",
+    secondary: "#BE74F9",
+    tertiary: "#E2BDFF",
+    quaternary: "#F5E8FF"
+  }
+};
+export {
+  colors,
+  text
+};
