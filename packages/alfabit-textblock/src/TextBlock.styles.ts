73ae2ceae7075d0e0a2d2187d00974426e0ec6cd
brenno luan <brennoluan8530@gmail.com>
2026-06-03 19:33:39 -0300

    chore: rename package to @brennoluan/alfabit-textblock and add initial styles for TextBlock component

---
+++ packages/alfabit-textblock/src/TextBlock.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  padding: 32px 24px 32px 48px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  border-radius: 8px;
+`;
