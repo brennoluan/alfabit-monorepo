c01e592535e9f95f80c00a5f8e0cb0641364ff5b
brenno luan <brennoluan8530@gmail.com>
2026-06-03 18:52:21 -0300

    feat: enhance createComponent script with error handling and directory validation

--- scripts/create-component.js
+++ scripts/create-component.js
@@ -9,5 +9,33 @@ async function createComponent(componentName) {
       "../packages",
       `alfabit-${componentName.toLowerCase()}`,
     );
+
+    if (!fs.existsSync(templateDir)) {
+      throw new Error(
+        `Não foi encontrado o diretório de template ${targetDir}`,
+      );
+    }
+
+    if (fs.existsSync(targetDir)) {
+      throw new Error(`O diretório do componente ${targetDir} já existe`);
+    }
+
+    await fs.copy(templateDir, targetDir);
+    console.log(`Template copiado com sucesso para ${targetDir}`);
+
+    const files = await fs.readdir(targetDir);
+
+    for (const file of files) {
+      const filePath = path.join(targetDir, file);
+      const stat = await fs.stat(filePath);
+
+      if (stat.isDirectory()) {
+        const subFiles = await fs.readdir(filePath);
+        files.push(...subFiles.map((subFile) => path.join(file, subFile)));
+        continue;
+      }
+    }
+  } catch (error) {
+    console.error(`Erro ao criar o componente ${componentName}:`, error);
   }
 }
