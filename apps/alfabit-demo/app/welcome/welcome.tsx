7ad3561cdec7e658d893e64a0b226715b1ee6e73
brenno luan <brennoluan8530@gmail.com>
2026-06-03 21:22:34 -0300

    feat: enhance Welcome component with dynamic theme-based styling and custom theme button

--- apps/alfabit-demo/app/welcome/welcome.tsx
+++ apps/alfabit-demo/app/welcome/welcome.tsx
@@ -1,17 +1,25 @@
 import { Button } from "@brennoluan/alfabit-button";
 import { darkTheme, lightTheme, type Theme } from "@brennoluan/alfabit-tokens";
 import { useContext } from "react";
+import { customTheme } from "~/config/themes";
 import { ThemeContext } from "~/context/themeContext";
 
 export function Welcome() {
   const { theme, changeTheme } = useContext(ThemeContext);
+  const buttonText = `Trocar para o tema ${theme === lightTheme || !theme ? "dark" : "light"}`;
 
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
+    <main
+      className="flex items-center justify-center pt-16 pb-4"
+      style={{ backgroundColor: theme?.colors.background }}
+    >
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
-            <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
+            <h1
+              className="leading text-2xl font-bold text-gray-800 dark:text-gray-100"
+              style={{ color: theme?.colors.text }}
+            >
               Bem vindo ao Design System Alfabit
             </h1>
           </div>
@@ -23,7 +31,10 @@ export function Welcome() {
             changeTheme(theme === lightTheme || !theme ? darkTheme : lightTheme)
           }
         >
-          Trocar o tema
+          {buttonText}
+        </Button>
+        <Button variant="secondary" onClick={() => changeTheme(customTheme)}>
+          Trocar para o tema customizado
         </Button>
       </div>
     </main>
