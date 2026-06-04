520ac028898202aef07feced3a32a22ec782e1f4
brenno luan <brennoluan8530@gmail.com>
2026-06-03 21:15:33 -0300

    feat: implement ThemeContext and ThemeProvider for centralized theme management

---
+++ apps/alfabit-demo/app/context/themeContext.tsx
@@ -0,0 +1,21 @@
+import type { Theme } from "@brennoluan/alfabit-tokens";
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext<{
+  theme: Theme | undefined;
+  changeTheme: (theme: Theme) => void;
+}>({
+  theme: undefined,
+  changeTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [appTheme, setAppTheme] = useState<Theme | undefined>(undefined);
+  return (
+    <ThemeContext.Provider
+      value={{ theme: appTheme, changeTheme: setAppTheme }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
